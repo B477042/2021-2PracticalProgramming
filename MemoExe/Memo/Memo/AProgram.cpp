237e89ad65a55e4b870ec842b4ae74e67463ff27
B477042 <37619106+B477042@users.noreply.github.com>
2021-12-19 00:56:49 +0900

    main Thread 생성 중

--- MemoExe/Memo/Memo/AProgram.cpp
+++ MemoExe/Memo/Memo/AProgram.cpp
@@ -1,1 +1,78 @@
 #include "AProgram.h"
+
+AProgram::AProgram()
+{
+}
+
+AProgram::~AProgram()
+{
+}
+
+void AProgram::StartProgram()
+{
+	init();
+	run();
+	closing();
+}
+
+void AProgram::init()
+{
+
+}
+
+void AProgram::run()
+{
+	startThreads();
+}
+
+void AProgram::closing()
+{
+
+}
+
+void AProgram::startThreads() 
+{
+	/*
+	* Thread는 총 5가지
+	* Main이 종료되면 프로그램이 종료되게 
+	* Main이 시작되면 4개의 Thread를 생성하게 만들기
+	* 종료 조건은 Main이 종료될 때
+	*/
+	hThread[Idx_Main] = (HANDLE)
+		_beginthreadex(
+			NULL,
+			0,
+			AProgram::procMainThread,
+			(LPVOID)(this),
+			CREATE_SUSPENDED,
+			(unsigned*)&dwThreadId[Idx_Main]
+		);
+
+	ResumeThread(hThread[Idx_Main]);
+
+
+	WaitForSingleObject(hThread[Idx_Main], INFINITE);
+
+}
+
+unsigned int __stdcall AProgram::procMainThread(LPVOID InputParam)
+{
+	auto This = (AProgram*)InputParam;
+	if (!This)
+	{
+		cout << "Casting FAiled\n";
+		delete This;
+		return -1;
+	}
+
+	/*
+	* 4가지 쓰레드를 동작 시킨다.
+	*/
+
+
+
+
+	return 0;
+}
+
+
--- MemoExe/Memo/Memo/AProgram.h
+++ MemoExe/Memo/Memo/AProgram.h
@@ -3,6 +3,31 @@
 #include"AReader.h"
 #include"ARecorder.h"
 #include"AInput.h"
+#include<unordered_map>
+#include <thread>
+#include <Windows.h>
+#include <process.h>
+using namespace std;
+
+//Thread Index 관련 메크로
+#define  Num_of_Thread 5
+#define  Idx_Main 0
+#define  Idx_Recorder  1
+#define  Idx_Printer  2
+#define  Idx_Reader  3
+#define  Idx_Writer  4
+
+
+//Thread에 파라메터로 보내는 구조체
+template<typename T>
+struct FThreadParam
+{
+public:
+	FThreadParam(T* Data):Data(Data){}
+	T* Data;
+	AProgram Program;
+
+};
 
 class AProgram
 {
@@ -10,13 +35,39 @@ public:
 	AProgram();
 	~AProgram();
 
-	void StartProgram()const;
+	void StartProgram();
 
 private:
-	void init();
+	//==============================================
+	// 프로그램 메인 함수들
+	// 
+	//쓰레드와 프로그램 정보 초기화
+	void init() ;
+	//메인 루프 실행
+	void run();
+	//프로그램 종료 단계
+	void closing();
+	//==============================================
+	// 쓰레드 관련 함수들
+	//Thread를 실행
+	void startThreads();
+	//Thread를 종료
+	void turnOffThreads();
+	//Main Thread 함수
+	static unsigned int WINAPI procMainThread(LPVOID InputParam);
+
+	// 크리티컬 섹션 필요하면
+
+	//==============================================
+	// 
+
+
 
 private:
 
+
+	//==============================================
+	// 클래스 객체
 	//메모 저장 
 	ARecorder* Recorder;
 	// 출력 관리
@@ -26,5 +77,12 @@ private:
 	// 사용자 인풋
 	AInput* Writer;
 
+	//저장된 파일 이름들 테이블, 있다면 True
+	unordered_map<string,bool> FileNameTable;
+
+	//===============================================
+	//쓰레드 관련
+	HANDLE hThread[Num_of_Thread];
+	DWORD dwThreadId[Num_of_Thread];
 };
 
